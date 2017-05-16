b72637c0bffa5cb571a37f88aff755a126264140
mikesmall <michael.l.a.small@gmail.com>
2017-05-16 15:10:59 -0400

    Exercise 0 complete

---
+++ exercises.rb
@@ -0,0 +1,28 @@
+fave_colours_strings = ["red", "green", "blue"]
+brothers_ages = [36, 33]
+coin_flips = ["heads", "heads", "tails", "heads", "tails"]
+fave_artists = ["Spinal Tap", "Anvil", "Orion"]
+fave_colours_symbols = [:red, :green, :blue]
+
+three_words = {
+  :paper => "flattened skinny wood pulp",
+  :candy => "popular edible sugar",
+  :porch => "a place for sitting"
+}
+
+fave_movie_years = {
+  :Jaws => 1975,
+  :Rocky => 1976,
+  :UHF => 1989
+}
+
+city_populations = {
+  :Toronto => 2731571,
+  :Halifax => 403131,
+  :Calgary => 1214839
+}
+
+brothers = {
+  :Scott => 33,
+  :Shaun => 36
+}
