677fffbbe7575e9587c189fcda09a74fb1a45ade
mikesmall <michael.l.a.small@gmail.com>
2017-05-16 18:25:58 -0400

    Completed exercise 6A

--- exercises.rb
+++ exercises.rb
@@ -126,3 +126,46 @@ end
 fave_colours_strings << "purple"
 fave_colours_strings << "yellow"
 puts fave_colours_strings
+
+# EXERCISE 6A
+# Make a new hash that contains a list of movies for each year. Each list of movies should be an array. Below is some data you can use.
+    # 1999: The Matrix, Star Wars: Episode 1, The Mummy
+    # 2009: Avatar, Star Trek, District 9
+    # 2019: How to Train Your Dragon 3, Toy Story 4, Star Wars: Episode 9
+blockbusters = {
+  1999 => ["The Matrix", "Star Wars: Episode 1", "The Mummy"],
+  2009 => ["Avatar", "Star Trek", "District 9"],
+  2019 => ["How to Train Your Dragon 3", "Toy Story 4", "Star Wars: Episode 9"]
+}
+puts blockbusters
+
+# Make a new array that contains each row of the buttons on a phone. Each row should be an array.
+    #The rows on a phone are: 1 2 3, 4 5 6, 7 8 9, * 0 #
+telephone = [
+  bottom_row = ["*", 0, "#"],
+  lower_row = [1, 2, 3],
+  upper_row = [4, 5, 6],
+  top_row = [7, 8, 9]
+]
+puts telephone
+
+#Make a new array that contains information about three countries. Each country should be a hash that has a name, a continent, and whether or not it is an island.
+
+countries = [
+  canada = {
+    :name => "Canada",
+    :continent => "North America",
+    :island => false
+  },
+  usa = {
+    :name => "United States",
+    :continent => "North America",
+    :island => false
+  },
+  iceland = {
+    :name => "Iceland",
+    :continent => "Europe",
+    :island => true
+  },
+]
+puts countries
